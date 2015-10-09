3482763f8ae2f0821bb5716e5854297169ece620
Stanislav Nechutný <stanislav@nechutny.net>
2015-10-09 22:01:09 +0200

    Prni verze volani pomoci

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@ $(document).ready(function(){
 	$("#menu_pick").on('click',pick.up);
 
 	$("#menu_phone").on('click',phone.open);
+
+	$("#menu_help").on('mousedown',phone.help.mousedown);
+	$("#menu_help").on('mouseup',phone.help.mouseup);
+	$("#menu_help").on('mouseout',phone.help.mouseout);
 });
 
 pick = {
 	'up' : function(){
-		
+
 	}
 }
--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,51 @@
 phone = {
 	open : function() {
 
+	},
+
+	help : {
+		interval: -1,
+		count: 0,
+
+		mousedown: function(e){
+			if(phone.help.interval == -1)
+			{
+				phone.help.count = -1;
+				phone.help.interval = setInterval(phone.help.tick, 1000);
+				phone.help.tick();
+			}
+
+		},
+
+		mouseup: function(){
+			if(phone.help.interval != -1)
+			{
+				if(phone.help.count == 0)
+				{
+					alert('Stiskněte 3 vteřiny.')
+				}
+				clearInterval(phone.help.interval);
+				phone.help.interval = -1;
+			}
+			$("#countDown").hide();
+		},
+
+		tick: function(){
+			phone.help.init();
+			phone.help.count++;
+			$("#countDown").html(3-phone.help.count);
+			$("#countDown").show();
+			if(3-phone.help.count == 0)
+			{
+				alert('Volám...');
+			}
+		},
+
+		init: function(){
+			if($("#countDown").length == 0)
+			{
+				$("body").append("<div id='countDown'></div>");
+			}
+		}
 	}
 };
